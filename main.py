c75ed2481c71837ab71fc73a10b45c11cfaa473f
alialkhiame <32980384+alialkhiame@users.noreply.github.com>
2023-11-10 17:07:03 +0100

    first Commit

---
+++ main.py
@@ -0,0 +1,40 @@
+# Import necessary libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+# Load data from a CSV file
+file_path = 'your_data.csv'  # Replace 'your_data.csv' with the actual path to your CSV file
+data = pd.read_csv(file_path)
+
+# Assuming your CSV has columns 'X' and 'y', change these column names based on your actual data
+X = data[['X']]
+y = data['y']
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create a linear regression model
+model = LinearRegression()
+
+# Train the model on the training data
+model.fit(X_train, y_train)
+
+# Make predictions on the test data
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+mse = mean_squared_error(y_test, y_pred)
+rmse = np.sqrt(mse)
+print(f"Root Mean Squared Error: {rmse}")
+
+# Plot the data and the regression line
+plt.scatter(X_test, y_test, color='black')
+plt.plot(X_test, y_pred, color='blue', linewidth=3)
+plt.xlabel('X')
+plt.ylabel('y')
+plt.title('Linear Regression')
+plt.show()
