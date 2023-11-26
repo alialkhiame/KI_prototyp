ac7177c7ad45ee278963cd87ca50b16f1bfef50f
alialkhiame <32980384+alialkhiame@users.noreply.github.com>
2023-11-26 15:30:13 +0100

    ki

--- app.py
+++ app.py
@@ -1,65 +1,82 @@
+import json
+
 from flask import Flask, request, render_template, jsonify
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 import io
 import matplotlib.pyplot as plt
 import base64
 from io import BytesIO
-# Load default data
-from flask import Flask
-# Import your dataGen class
-from dataGen import dataGen
 
 app = Flask(__name__)
-
-
-
-app = Flask(__name__)
-
-# Create an instance of the dataGen class
-data_generator = dataGen()
-
-# Use the generate_data method to initialize default_data
-default_data = data_generator.generate_data()
-
-# ... rest of your Flask app code ...
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')  # HTML file with upload and selection options
+    return render_template('index.html')
+
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    if 'file' in request.files:
-        file = request.files['file']
-        data = pd.read_csv(file.stream)
-    else:
-        data = default_data
+    if 'file' not in request.files:
+        return jsonify(error="No file part"), 400
 
-    # Send column names to frontend for variable selection
-    return jsonify(columns=data.columns.tolist())
+    file = request.files['file']
+
+    try:
+        data = pd.read_csv(file.stream)
+        mean_value = data['Umsatz'].mean()
+        cleaned_data = data.fillna(mean_value)
+
+    except Exception as e:
+        return jsonify(error=str(e)), 400
+
+    return jsonify(columns=cleaned_data.columns.tolist())
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = pd.read_csv(io.StringIO(request.form['data']))
-    selected_columns = request.form.getlist('selected_columns')
-    X = data[selected_columns]
-    y = data['target_column']  # Define your target column
+    try:
+        data = pd.read_csv(request.files['data'])
+        selected_columns = request.form['selected_columns']
+        target_column = request.form['target_column']
 
-    model = LinearRegression()
-    model.fit(X, y)
+        mean_value = data['Umsatz'].mean()
+        cleaned_data = data.fillna(mean_value)
 
-    # Predictions and plotting
-    predictions = model.predict(X)
-    plt.figure()
-    plt.plot(data['some_time_column'], predictions)  # Modify as needed
-    img = BytesIO()
-    plt.savefig(img, format='png')
-    img.seek(0)
-    plot_url = base64.b64encode(img.getvalue()).decode()
+        # Convert JSON string back to list
+        selected_columns = json.loads(selected_columns)
+        print(selected_columns)
+        # Validate data
+        if not all(str(column) in map(str, cleaned_data.columns) for column in selected_columns + [target_column]):
+            print(cleaned_data.columns)
+            return jsonify(error="Some columns are missing"), 400
 
-    return jsonify(predictions=predictions.tolist(), plot_url=plot_url)
+        X = cleaned_data[selected_columns]
+        y = cleaned_data[target_column]
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+        model = LinearRegression()
+        model.fit(X_train, y_train)
+
+        predictions = model.predict(X_test)
+
+        # Plotting
+        plt.figure()
+        plt.scatter(X_test.iloc[:, 0], y_test)
+        plt.plot(X_test.iloc[:, 0], predictions, color='red')
+        img = BytesIO()
+        plt.savefig(img, format='png')
+        img.seek(0)
+        plot_url = base64.b64encode(img.getvalue()).decode()
+
+        return jsonify(predictions=predictions.tolist(), plot_url=plot_url)
+    except Exception as e:
+        return jsonify(error=str(e)), 400
+
 
 if __name__ == '__main__':
+    print("KI_")
     app.run(debug=True)
